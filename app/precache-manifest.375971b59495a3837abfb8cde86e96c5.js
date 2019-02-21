6d31a4ea58b2d0845ade545fa6059d8fd95c8aab
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-02-21 18:17:55 +0200

    builded

---
+++ app/precache-manifest.375971b59495a3837abfb8cde86e96c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d8afb2e0a62511da5fa",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.8d8afb2e.js"
+  },
+  {
+    "revision": "abe4eeb88eac5fd20be7",
+    "url": "/tic-tac-toe-react/static/js/main.abe4eeb8.chunk.js"
+  },
+  {
+    "revision": "2dc58e7f2f8b7cb434f6",
+    "url": "/tic-tac-toe-react/static/js/2.2dc58e7f.chunk.js"
+  },
+  {
+    "revision": "abe4eeb88eac5fd20be7",
+    "url": "/tic-tac-toe-react/static/css/main.dfe2c712.chunk.css"
+  },
+  {
+    "revision": "49a76452ead77ce9525800c46bfa2d7b",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
