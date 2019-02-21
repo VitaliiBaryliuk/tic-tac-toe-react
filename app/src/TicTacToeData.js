0887010e11f840f1adc1863b7caadf0083947ccb
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-02-21 17:36:18 +0200

    init commit

---
+++ app/src/TicTacToeData.js
@@ -0,0 +1,40 @@
+const ticTacToeData = [
+  {
+    cellNum: 0,
+    value: null,
+  },
+  {
+    cellNum: 1,
+    value: null,
+  },
+  {
+    cellNum: 2,
+    value: null,
+  },
+  {
+    cellNum: 3,
+    value: null,
+  },
+  {
+    cellNum: 4,
+    value: null,
+  },
+  {
+    cellNum: 5,
+    value: null,
+  },
+  {
+    cellNum: 6,
+    value: null,
+  },
+  {
+    cellNum: 7,
+    value: null,
+  },
+  {
+    cellNum: 8,
+    value: null,
+  },
+];
+
+export default ticTacToeData;
